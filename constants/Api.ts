75f7295927b9db4c9edff1b2aea0ea04bb781e17
Daniel Clancy <clancy.daniel@gmail.com>
2024-12-26 22:06:52 -0600

    Removed old app URL

--- constants/Api.ts
+++ constants/Api.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = "http://10.0.2.2:3000/";
-export const BASE_URL = "https://dolphin-app-6uscb.ondigitalocean.app/";
+export const BASE_URL = "http://10.0.2.2:3000/";
 
 export const GET_LOCATIONS_API = BASE_URL + "locations/all";
 export const GET_LOCATION_WITH_DATA_BY_ID = BASE_URL + "locations/1/data";
